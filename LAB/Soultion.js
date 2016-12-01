b25d2ea70f5f9160e93cb1950556a9ffe6ee1988
Muhammad Shehroz Sajjad <shahrozt20@gmail.com>
2016-12-01 15:44:26 +0500

    Version 1.1
    Basic expression validation and calculation

---
+++ LAB/Soultion.js
@@ -0,0 +1,23 @@
+﻿document.getElementById("evaluate").onclick = function () {
+    mathEval(document.getElementById("equation").value);
+}
+function mathEval(exp) {
+    var reg = /(?:[a-z$_][a-z0-9$_]*)|(?:[;={}\[\]"'!&<>^\\?:])/ig,
+        valid = true;
+
+    // Detect valid JS identifier names and replace them
+    exp = exp.replace(reg, function ($0) {
+        // If the name is a direct member of Math, allow
+        if (Math.hasOwnProperty($0))
+            return "Math." + $0;
+            // Otherwise the expression is invalid
+        else
+            valid = false;
+    });
+
+    // Don't eval if our replace function flagged as invalid
+    if (!valid)
+        alert("Invalid arithmetic expression");
+    else
+        try { alert(eval(exp)); } catch (e) { alert("Invalid arithmetic expression"); };
+}
